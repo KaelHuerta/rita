e66583c1ca91daddaf001b573b4a13e1fb665fac
KaelHuerta <kaelhuerta@gmail.com>
2014-03-30 20:26:41 -0600

    corrigiendo tabla alfie

--- alfie/alfie_insert.sql
+++ alfie/alfie_insert.sql
@@ -98,62 +98,63 @@
 --      AND uniquecarrier IS NOT NULL
 --	);
 
-WITH uniqdestinations AS (
-  SELECT uniquecarrier, COUNT(DISTINCT dest) AS uniqdest
-  FROM clean.rita
-    WHERE flightdate >=  DATE (:v1 || '-01-01')
-      AND flightdate < DATE ((:v1+1) ||'-01-01')
-      AND uniquecarrier IS NOT NULL
-  GROUP BY uniquecarrier
-);
-
 INSERT INTO alfie.alfie (
-	SELECT 
-	    uniquecarrier
-	   ,tailnum
-	   ,extract(year FROM flightdate) AS year
-	   ,extract(month FROM flightdate) AS month
-	   ,first_value(flightdate) over (
-	   		partition by
-	   	   	    uniquecarrier
-	   	       ,tailnum
-		    ORDER BY flightdate
-		) AS firsttail
-	   ,last_value(flightdate) over (
-			partition by
-			    uniquecarrier
-			   ,tailnum
-		    ORDER BY flightdate
-		) AS lasttail
-	   ,avg(coalesce(arrdelay, 0)) over (
-		    partition by
-		        uniquecarrier
-		       ,tailnum
-		       ,extract(month FROM flightdate)
-		) AS delaymonth
-	   ,sum(distance) over (
-		    partition by
-		        uniquecarrier
-		       ,extract(year FROM flightdate)
-		) AS kmcarrier
-	   ,sum(coalesce(airtime, 0)) over (
-	   	    partition by
-	   	        uniquecarrier
-	   	) AS flytimecarrier
-	   ,sum(1) over (
-	   	    partition by
-	   	        uniquecarrier
-	   	    ) AS flightscarrier
-	   ,avg(arrdelay) over (
-	   	    partition by
-	   	        uniquecarrier
-	   	       ,tailnum
-	   	       ,extract(month FROM flightdate)
-	   	) as avgmonthdelay
-        b.uniqdest AS numdest
-	FROM clean.rita
-	NATURAL JOIN uniqdestinations
-	WHERE flightdate >=  DATE (:v1 || '-01-01')
-      AND flightdate < DATE ((:v1+1) ||'-01-01')
-      AND uniquecarrier IS NOT NULL
-	);
+   SELECT 
+       uniquecarrier
+      ,tailnum
+      ,extract(year FROM flightdate) AS year
+      ,extract(month FROM flightdate) AS month
+      ,first_value(flightdate) over (
+            partition by
+                 uniquecarrier
+                ,tailnum
+                ,extract(year from flightdate)
+          ORDER BY flightdate
+      ) AS firsttail
+      ,last_value(flightdate) over (
+         partition by
+             uniquecarrier
+            ,tailnum
+          ORDER BY flightdate
+      ) AS lasttail
+      ,avg(coalesce(arrdelay, 0)) over (
+          partition by
+              uniquecarrier
+             ,tailnum
+             ,extract(year from flightdate)
+             ,extract(month FROM flightdate)
+      ) AS delaymonth
+      ,sum(distance) over (
+          partition by
+              uniquecarrier
+             ,extract(year FROM flightdate)
+      ) AS kmcarrier
+      ,sum(coalesce(airtime, 0)) over (
+             partition by
+                 uniquecarrier
+                ,extract(year from flightdate)
+         ) AS flytimecarrier
+      ,sum(1) over (
+             partition by
+                 uniquecarrier
+                ,extract(year from flightdate)
+             ) AS flightscarrier
+      ,avg(arrdelay) over (
+             partition by
+                 uniquecarrier
+                ,tailnum
+                ,extract(year from flightdate)
+                ,extract(month FROM flightdate)
+         ) as avgmonthdelay
+      ,uniqdest AS numdest
+   FROM clean.rita
+   NATURAL JOIN (
+       SELECT uniquecarrier, COUNT(DISTINCT dest) AS uniqdest
+       FROM clean.rita
+       WHERE flightdate >=  DATE (2003 || '-01-01')
+         AND flightdate < DATE (2003 ||'-01-01')
+         AND uniquecarrier IS NOT NULL
+       GROUP BY uniquecarrier) AS tmp
+   WHERE flightdate >=  DATE (2003 || '-01-01')
+      AND flightdate < DATE ((2003+1) ||'-01-01')
+);

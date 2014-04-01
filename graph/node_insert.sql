2fcfe6003ef2eaaf4906d28558b4dee330d04f07
KaelHuerta <kaelhuerta@gmail.com>
2014-04-01 07:01:21 -0600

    corrigiendo errores

--- graph/node_insert.sql
+++ graph/node_insert.sql
@@ -12,7 +12,8 @@ INSERT INTO graph.nodes (
   FROM clean.airports AS a
   LEFT JOIN (
     SELECT
-        origin, count(1) AS departures
+        origin
+       ,count(*) AS departures
     FROM clean.rita
     WHERE flightdate >=  DATE (:v1 || '-01-01')
       AND flightdate < DATE ((:v1+1) ||'-01-01')
@@ -21,10 +22,47 @@ INSERT INTO graph.nodes (
   ON a.iata = b.origin
   LEFT JOIN (
     SELECT
-        dest, count(1) AS arrivals
+        dest
+       ,count(*) AS arrivals
     FROM clean.rita
     WHERE flightdate >=  DATE (:v1 || '-01-01')
       AND flightdate < DATE ((:v1+1) ||'-01-01')
+    GROUP BY dest
   ) AS c
   ON a.iata = c.dest
 );
+
+--
+--
+--explain
+--  SELECT
+--      2013 AS year
+--     ,iata
+--     ,airportname
+--     ,city
+--     ,country
+--     ,latitude
+--     ,longitude
+--     ,b.departures AS departures
+--     ,c.arrivals AS arrivals
+--  FROM clean.airports AS a
+--  LEFT JOIN (
+--    SELECT
+--        origin
+--       ,count(*) AS departures
+--    FROM clean.rita
+--    WHERE flightdate >=  DATE (2013 || '-01-01')
+--      AND flightdate < DATE ((2013+1) ||'-01-01')
+--    GROUP BY origin
+--  ) AS b
+--  ON a.iata = b.origin
+--  LEFT JOIN (
+--    SELECT
+--        dest
+--       ,count(*) AS arrivals
+--    FROM clean.rita
+--    WHERE flightdate >=  DATE (2013 || '-01-01')
+--      AND flightdate < DATE ((2013+1) ||'-01-01')
+--    GROUP BY dest
+--  ) AS c
+--  ON a.iata = c.dest;

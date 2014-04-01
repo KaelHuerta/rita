6484a85960f678efca0f457a2f1af527645d9133
KaelHuerta <kaelhuerta@gmail.com>
2014-04-01 06:58:29 -0600

    añadiendo grafo

--- graph/edge_insert.sql
+++ graph/edge_insert.sql
@@ -1,27 +1,28 @@
-SELECT
-    year
-   ,origin
-   ,dest
-   ,count(*) AS weight
-   ,sum(cancellation) AS cancellations
-   ,sum(diverted) AS deviations
-   ,sum(CASE WHEN arrdelay > 0 THEN 1 ELSE 0) AS numdelay
-   ,sum(CASE WHEN arrdelay = 0 THEN 1 ELSE 0) AS numontime
-   ,sum(CASE WHEN arrdelay < 0 THEN 1 ELSE 0) AS numearly
-   ,avg(depdelay) AS avgdepdelay
-   ,avg(arrdelay) AS avgarrdelay
-   ,avg(distance) AS distance
-   ,avg(airtime) AS airtime
-   ,avg(carrierdelay) AS avgcarrierdelay
-   ,avg(weatherdelay) AS avgweatherdelay
-   ,avg(nasdelay) AS avgnasdelay
-   ,avg(securitydelay) AS avgsecuritydelay
-   ,avg(lateaircraftdelay) AS avglateplanelay
-FROM clean.rita
-WHERE flightdate >=  DATE (:v1 || '-01-01')
-  AND flightdate < DATE ((:v1+1) ||'-01-01')
-GROUP BY
-    year
-   ,origin
-   ,dest
-;
+INSERT INTO graph.edges (
+  SELECT
+      :v1 as year
+     ,origin
+     ,dest
+     ,count(*) AS weight
+     ,sum(cancelled) AS cancellations
+     ,sum(diverted) AS deviations
+     ,sum(CASE WHEN arrdelay > 0 THEN 1 ELSE 0 END) AS numdelay
+     ,sum(CASE WHEN arrdelay = 0 THEN 1 ELSE 0 END) AS numontime
+     ,sum(CASE WHEN arrdelay < 0 THEN 1 ELSE 0 END) AS numearly
+     ,avg(depdelay) AS avgdepdelay
+     ,avg(arrdelay) AS avgarrdelay
+     ,avg(distance) AS distance
+     ,avg(airtime) AS airtime
+     ,avg(carrierdelay) AS avgcarrierdelay
+     ,avg(weatherdelay) AS avgweatherdelay
+     ,avg(nasdelay) AS avgnasdelay
+     ,avg(securitydelay) AS avgsecuritydelay
+     ,avg(lateaircraftdelay) AS avglateplanelay
+  FROM clean.rita
+  WHERE flightdate >=  DATE (:v1 || '-01-01')
+    AND flightdate < DATE ((:v1+1) ||'-01-01')
+  GROUP BY
+      year
+     ,origin
+     ,dest
+);
